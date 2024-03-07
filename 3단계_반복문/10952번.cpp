203c74331c57b37d787eb6892e8d2379bfacd1be
xogns <xogns40430@naver.com>
2024-03-07 12:12:28 +0900

    A+B - 5

---
+++ 3단계_반복문/10952번.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	while (true)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		if (a <= 0 && b <= 0)
+			break;
+		
+		cout << a + b << endl;
+	}
+
+	return 0;
+}

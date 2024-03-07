13b1b3af89cb958956efb41bd5ed8759d27918ab
xogns <xogns40430@naver.com>
2024-03-07 11:21:30 +0900

    A+B - 7

---
+++ 3단계_반복문/11021번.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	int x;
+	cin >> x;
+	
+	for (int i = 1; i <= x; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << endl;
+	}
+	
+	return 0;
+}

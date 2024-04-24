e4f188af51f0a5d64ffd13490867e311557775ff
xogns <xogns40430@naver.com>
2024-04-24 11:37:13 +0900

    단어 공부

--- 심화1/1157번.cpp
+++ 심화1/1157번.cpp
@@ -68,3 +68,36 @@ int main(void)
 	
 	return 0;
 }
+
+// 더 좋은 코드
+// #include <stdio.h>
+// #include <string.h>
+
+// char a[1000005];
+// int arr[26];
+
+// int main(){
+//     int max = 0;
+//     int size;
+//     char ans;
+
+//     scanf("%s",a);
+//     size = strlen(a);
+
+//     for(int i=0;i<size;i++)
+//         if(a[i]>='a') arr[a[i]-'a']++;
+//         else arr[a[i]-'A']++;
+
+//     for(int i=0;i<26;i++)
+//         if(arr[i]==max){
+//             ans = '?';
+//         }
+//         else if(arr[i]>max){
+//             max = arr[i];
+//             ans = 'A'+i;
+//         }
+
+//     printf("%c",ans);
+
+//     return 0;
+// }

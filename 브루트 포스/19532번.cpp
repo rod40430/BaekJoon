35d227438542700e93e26e2a9caa2f9bb79103da
xogns <xogns40430@naver.com>
2024-11-13 11:26:28 +0900

    다른 방식 추가

--- 브루트 포스/19532번.cpp
+++ 브루트 포스/19532번.cpp
@@ -11,3 +11,33 @@ int main(void)
     
     return 0;
 }
+
+// 여러 방법이 있다
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void)
+// {
+//     int a, b, c, d, e, f;
+//     cin >> a >> b >> c >> d >> e >> f;
+
+//     int x, y;
+//     x = a + d;
+//     y = b + e;
+
+
+// 	for (int x = -999; x <= 999; x++)
+// 	{
+// 		for (int y = -999; y <= 999; y++)
+// 		{
+// 			if ((a * x + b * y == c) && (d * x + e * y == f))
+// 			{
+// 				cout << x << " " << y;
+// 				break;
+// 			}
+// 		}
+// 	}
+    
+//     return 0;
+// }

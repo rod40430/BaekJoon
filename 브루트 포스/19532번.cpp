91fd9ebc16307b3e784313c0ed71231bbd80f68a
xogns <xogns40430@naver.com>
2024-11-13 11:15:37 +0900

    수학은 비대면강의입니다
    머리가 좋으면 몸이 고생안한다.
    몸이 좋으면 머리가 고생안한다.

---
+++ 브루트 포스/19532번.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    cout << (c * e - b * f) / (a * e - b * d) << ' ' << (c * d - a * f) / (b * d - a * e);
+    
+    return 0;
+}
